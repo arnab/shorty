90e7ec90ec95aefda2673134eefbc50ae6984e6f
Arnab Deka <arnab.deka@gmail.com>
2013-07-16 12:42:08 +0530

    enable admin

--- shorty/urls.py
+++ shorty/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -13,5 +14,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
